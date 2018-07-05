082d1af9fdedbc94296232c62caa1c17ed6e9bf8
Ivan Pankrashin <pankrashin.ivan@gmail.com>
2018-07-05 17:31:39 +0200

    correct jar file *debug lines in comments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,6 @@
 package com.company;
 
 import java.awt.*;
-import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
@@ -9,7 +8,7 @@ public class Main {
     public static void main(String[] args)
             throws Exception
     {
-        System.out.println("Main executed");
+        System.out.println("'Moving' executed");
 
         Robot robot = new Robot();
         int curX = 0, curY = 0, prevX, prevY;
@@ -19,6 +18,15 @@ public class Main {
             prevY = curY;
             curX = (int)MouseInfo.getPointerInfo().getLocation().getX();
             curY = (int)MouseInfo.getPointerInfo().getLocation().getY();
+//            System.out.print("prevX: ");
+//            System.out.println(prevX);
+//            System.out.print("prevY: ");
+//            System.out.println(prevY);
+//            System.out.print("curX: ");
+//            System.out.println(curX);
+//            System.out.print("curY: ");
+//            System.out.println(curY);
+//            System.out.println("--------------------");
             robot.delay(15000);
             if(curX == prevX && curY == prevY) {
                 robot.mouseMove(ThreadLocalRandom.current().nextInt(0, 641), ThreadLocalRandom.current().nextInt(0, 481));
